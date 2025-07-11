1aa65d3e48eae90e7a5767fb0f6765a3499ff00a
hailen <soyhailen@gmail.com>
2025-07-11 01:24:14 -0500

    dcw

--- ParcialFinalAvanzada/src/main/java/edu/progavud/parcial3pa/Parcial3paApplication.java
+++ ParcialFinalAvanzada/src/main/java/edu/progavud/parcial3pa/Parcial3paApplication.java
@@ -3,10 +3,12 @@ package edu.progavud.parcial3pa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @SpringBootApplication
 public class Parcial3paApplication {
 
